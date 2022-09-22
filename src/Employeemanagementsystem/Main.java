ba16e3e6ed4f092b0b6cc3ba54c43d30ee5cc0d0
seunrae <oluseun.ademilua@gmail.com>
2022-09-22 15:37:52 +0100

    added get last name

--- src/Employeemanagementsystem/Main.java
+++ src/Employeemanagementsystem/Main.java
@@ -4,6 +4,7 @@ public  class Main {
     public static void main(String[] args) {
    Applicant applicant = new Applicant(001,"oluseun", "ademilua","oluseun.ademilua@gmail.com","05-21-2022",001,"05-21-2022","3-years");
         System.out.println(applicant.getFirstName());
+        System.out.println(applicant.getLastName());
 
 
 
